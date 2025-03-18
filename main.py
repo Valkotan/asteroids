61872831c075bdf4b88e33d429c29f1316d978cc
Valkotan <calisamuchastegui@gmail.com>
2025-03-17 22:52:37 -0700

    It wiggles

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from player import Player
+from player import *
 
 
 def main():
@@ -10,15 +10,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
+        #limit game FPS to 60
+        dt = clock.tick(60) / 1000
+
+        #Handle Events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Update game objects
+        player.update(dt)
+
+        # Draw everything
         screen.fill("black")
         player.draw(screen)
+        # Draw other game objects...
+
         pygame.display.flip()
 
-        #limit game FPS to 60
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
